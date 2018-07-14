0320228e45bc0e40be4d5a3b3f387bb55d6b4378
Andrew Berndt <abrnst@gmail.com>
2018-07-13 19:59:10 -0500

    its working

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        
+        res.render("index", hbsObject);
+    });
+});
+
+router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.updateOne({
+        devoured: true
+        
+    }, condition, function(data) {
+        res.redirect("/");
+    });
+});
+
+router.post("/burgers", function(req, res){
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function(result) {
+        res.redirect("/");
+    }); 
+});
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+
+
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Drew Burger", "no");
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Big Mac", "no");
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Sloppy Drew", "no");
